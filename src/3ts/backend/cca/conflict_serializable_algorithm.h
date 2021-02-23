5ff6b1104b167362e5574d3a86101d0bc70d27c8
williamcliu <williamcliu@tencent.com>
2021-02-23 10:15:48 +0800

    Fix unknown anomaly type in ConflictSerializableAlgorithm

--- src/3ts/backend/cca/conflict_serializable_algorithm.h
+++ src/3ts/backend/cca/conflict_serializable_algorithm.h
@@ -77,16 +77,16 @@ namespace ttts {
 
 class PreceInfo {
  public:
-  PreceInfo(const uint64_t trans_id, const uint64_t pre_trans_id, const uint64_t item_id, const PreceType type, const uint32_t pre_order, const uint32_t order)
-    : trans_id_(trans_id), pre_trans_id_(pre_trans_id), item_id_(item_id), type_(type), order_(order) {}
+  PreceInfo(const uint64_t pre_trans_id, const uint64_t trans_id, const uint64_t item_id, const PreceType type, const uint32_t order)
+    : pre_trans_id_(pre_trans_id), trans_id_(trans_id), item_id_(item_id), type_(type), order_(order) {}
   PreceInfo(const PreceInfo&) = default;
 
   friend std::ostream& operator<<(std::ostream& os, const PreceInfo prece) {
     return os << 'T' << static_cast<char>('0' + prece.pre_trans_id_) << "-[" << prece.type_ << "-"
               << static_cast<char>('a' + prece.item_id_) << "]->" << 'T' << static_cast<char>('0' + prece.trans_id_);
   }
-  bool operator>(const PreceInfo& p) const { return order_ > p.order_ || (order_ == p.order_ && pre_order_ > p.pre_order_); }
-  bool operator<(const PreceInfo& p) const { return order_ < p.order_ || (order_ == p.order_ && pre_order_ < p.pre_order_); }
+  bool operator>(const PreceInfo& p) const { return order_ > p.order_; }
+  bool operator<(const PreceInfo& p) const { return order_ < p.order_; }
 
   uint64_t item_id() const { return item_id_; }
   PreceType type() const { return type_; }
@@ -96,7 +96,6 @@ class PreceInfo {
   uint64_t pre_trans_id_;
   uint64_t item_id_;
   PreceType type_;
-  uint32_t pre_order_;
   uint32_t order_;
 };
 
@@ -106,14 +105,21 @@ class ConflictGraphNode {
   ~ConflictGraphNode() {}
 
   bool HasNoPreTrans() const { return pre_trans_set_.empty(); }
-  void AddPreTrans(const uint64_t pre_trans_id, const uint64_t item_id, const PreceType type, const uint32_t pre_order, const uint32_t order) {
+  void AddPreTrans(const uint64_t pre_trans_id, const uint64_t item_id, const PreceType type, const uint32_t order) {
     // we only record the first precedence between the two specific transactions
-    pre_trans_set_.emplace(pre_trans_id, PreceInfo{trans_id_, pre_trans_id, item_id, type, pre_order, order});
+    const auto& [it, inserted] = pre_trans_set_.try_emplace(pre_trans_id, PreceInfo{pre_trans_id, trans_id_, item_id, type, order});
+    const auto& current_prece = it->second;
+    if (!inserted && current_prece.item_id() == item_id && current_prece.type() == PreceType::WR && type == PreceType::WW) {
+      // WW precedence's priority is higher than WR precedence
+      // e.g. W1a R2a W2a R1a, the pattern should be W1W2-W2R1 but not W1R2-W2R1 which cannot be identified to an anomaly
+      it->second = PreceInfo(pre_trans_id, trans_id_, item_id, type, order);
+    }
   }
   void RemovePreTrans(const uint64_t pre_trans_id) { pre_trans_set_.erase(pre_trans_id); }
   void Remove() { removed_ = true; }
   bool IsRemoved() const { return removed_; }
   std::optional<bool>& is_committed() { return is_committed_; }
+  const std::optional<bool>& is_committed() const { return is_committed_; }
   const std::map<uint64_t, PreceInfo>& pre_trans_set() const { return pre_trans_set_; }
   uint64_t trans_id() const { return trans_id_; }
 
@@ -187,41 +193,21 @@ class ConflictGraph {
     }
   }
 
-  void Insert(const std::pair<uint64_t, uint32_t>& pre_trans_id_and_order, const std::pair<uint64_t, uint32_t>& trans_id_and_order,
-      const uint64_t item_id, PreceType type) {
-    const uint64_t pre_trans_id = pre_trans_id_and_order.first;
-    const uint64_t trans_id = trans_id_and_order.first;
+  template <PreceType TYPE>
+  void Insert(const uint64_t pre_trans_id, const uint64_t trans_id, const uint64_t item_id, const uint32_t order) {
     if (pre_trans_id == trans_id) {
       return;
     }
-    if (type == PreceType::WW || type == PreceType::WCW) {
-      if (!nodes_[pre_trans_id].is_committed().has_value()) {
-        type = PreceType::WW;
-      } else if (nodes_[pre_trans_id].is_committed().value()) {
-        type = PreceType::WCW;
-      } else {
-        return; // W1A2W1 does not have T2->T1 precedence
-      }
-    } else if (type == PreceType::WR || type == PreceType::WCR) {
-      if (!nodes_[pre_trans_id].is_committed().has_value()) {
-        type = PreceType::WR;
-      } else if (nodes_[pre_trans_id].is_committed().value()) {
-        type = PreceType::WCR;
-      } else {
-        return; // W1A2R1 does not have T2->T1 precedence
-      }
-    } else if ((type == PreceType::WA || type == PreceType::WC) &&
-        nodes_[pre_trans_id].is_committed().has_value() &&
-        !nodes_[pre_trans_id].is_committed().value()) {
-      return; // W1W2A2A1 and W1W2A2C1 do not have T2->T1 precedence
+    const auto type = RealPreceType_<TYPE>(pre_trans_id);
+    if (type.has_value()) {
+      nodes_[trans_id].AddPreTrans(pre_trans_id, item_id, *type, order);
     }
-    nodes_[trans_id].AddPreTrans(pre_trans_id, item_id, type, pre_trans_id_and_order.second, trans_id_and_order.second);
   }
 
-  void Insert(const std::map<uint64_t, uint32_t>& pre_trans_id_set, const std::pair<uint64_t, uint32_t>& trans_id_and_order,
-      const uint64_t item_id, const PreceType type) {
-    for (const auto& pre_trans_id_and_order : pre_trans_id_set) {
-      Insert(pre_trans_id_and_order, trans_id_and_order, item_id, type);
+  template <PreceType TYPE>
+  void Insert(const std::set<uint64_t>& pre_trans_id_set, const uint64_t trans_id, const uint64_t item_id, const uint32_t order) {
+    for (const uint64_t pre_trans_id : pre_trans_id_set) {
+      Insert<TYPE>(pre_trans_id, trans_id, item_id, order);
     }
   }
 
@@ -282,6 +268,28 @@ class ConflictGraph {
   }
 
  private:
+  std::optional<PreceType> RealPreceType_(const uint64_t pre_trans_id, const std::optional<PreceType>& active_prece_type,
+      const std::optional<PreceType>& committed_prece_type, const std::optional<PreceType>& aborted_prece_type) const {
+    if (!nodes_[pre_trans_id].is_committed().has_value()) {
+      return active_prece_type;
+    } else if (nodes_[pre_trans_id].is_committed().value()) {
+      return committed_prece_type;
+    } else {
+      return aborted_prece_type;
+    }
+  }
+
+  template <PreceType TYPE>
+  std::optional<PreceType> RealPreceType_(const uint64_t pre_trans_id) const {
+    if constexpr (TYPE == PreceType::WW) {
+      return RealPreceType_(pre_trans_id, PreceType::WW, PreceType::WCW, {});
+    } else if constexpr (TYPE == PreceType::WR) {
+      return RealPreceType_(pre_trans_id, PreceType::WR, PreceType::WCR, {});
+    } else {
+      return RealPreceType_(pre_trans_id, TYPE, TYPE, {});
+    }
+  }
+
   // keep remove nodes of which indegree is 0
   void RemoveNodesNotInCycle_() {
     bool removed_node = false;
@@ -337,24 +345,24 @@ class ConflictSerializableAlgorithm : public HistoryAlgorithm {
 
   virtual bool Check(const History& history, std::ostream* const os) const override {
     ConflictGraph graph(history.trans_num());
-    std::vector<std::map<uint64_t, uint32_t>> read_trans_set_for_items(history.item_num());
-    std::vector<std::map<uint64_t, uint32_t>> write_trans_set_for_items(history.item_num());
+    std::vector<std::set<uint64_t>> read_trans_set_for_items(history.item_num());
+    std::vector<std::set<uint64_t>> write_trans_set_for_items(history.item_num());
     std::vector<std::set<uint64_t>> write_item_set_for_transs(history.trans_num());
     for (size_t i = 0, size = history.size(); i < size; ++i) {
       const Operation& operation = history.operations()[i];
       const uint64_t trans_id = operation.trans_id();
       if (operation.IsPointDML()) {
         const uint64_t item_id = operation.item_id();
-        auto& read_trans_set = read_trans_set_for_items[item_id];
-        auto& write_trans_set = write_trans_set_for_items[item_id];
+        std::set<uint64_t>& read_trans_set = read_trans_set_for_items[item_id];
+        std::set<uint64_t>& write_trans_set = write_trans_set_for_items[item_id];
         if (Operation::Type::READ == operation.type()) {
-          graph.Insert(write_trans_set, {trans_id, i}, item_id, PreceType::WR); // WR or WCR is both possible
-          read_trans_set.emplace(trans_id, i);
+          graph.Insert<PreceType::WR>(write_trans_set, trans_id, item_id, i); // WR or WCR is both possible
+          read_trans_set.insert(trans_id);
         } else if (Operation::Type::WRITE == operation.type()) {
-          // TODO: the two different type precedences happens at the same time
-          graph.Insert(write_trans_set, {trans_id, i}, item_id, PreceType::WW); // WW or WCW is both possible
-          graph.Insert(read_trans_set, {trans_id, i}, item_id, PreceType::RW);
-          write_trans_set.emplace(trans_id, i);
+          // WW precedence's priority is higher than RW precedence
+          graph.Insert<PreceType::WW>(write_trans_set, trans_id, item_id, i); // WW or WCW is both possible
+          graph.Insert<PreceType::RW>(read_trans_set, trans_id, item_id, i);
+          write_trans_set.insert(trans_id);
           write_item_set_for_transs[trans_id].insert(item_id); // record for check WA or WC
         }
       } else if (Operation::Type::SCAN_ODD == operation.type()) {
@@ -362,14 +370,14 @@ class ConflictSerializableAlgorithm : public HistoryAlgorithm {
       } else if (Operation::Type::ABORT == operation.type()) {
         graph.is_committed(trans_id) = false;
         for (const uint64_t write_item : write_item_set_for_transs[trans_id]) {
-          // TODO: the two different type precedences happens at the same time
-          graph.Insert(read_trans_set_for_items[write_item], {trans_id, i}, write_item, PreceType::RA);
-          graph.Insert(write_trans_set_for_items[write_item], {trans_id, i}, write_item, PreceType::WA);
+          // WA precedence's priority is higher than RA precedence
+          graph.Insert<PreceType::WA>(write_trans_set_for_items[write_item], trans_id, write_item, i);
+          graph.Insert<PreceType::RA>(read_trans_set_for_items[write_item], trans_id, write_item, i);
         }
       } else if (Operation::Type::COMMIT == operation.type()) {
         graph.is_committed(trans_id) = true;
         for (const uint64_t write_item : write_item_set_for_transs[trans_id]) {
-          graph.Insert(write_trans_set_for_items[write_item], {trans_id, i}, write_item, PreceType::WC);
+          graph.Insert<PreceType::WC>(write_trans_set_for_items[write_item], trans_id, write_item, i);
         }
       }
     }
@@ -460,7 +468,7 @@ class ConflictSerializableAlgorithm : public HistoryAlgorithm {
   }
 
   mutable std::array<std::atomic<uint64_t>, AnomalyTypeCount> anomaly_counts_;
-  mutable uint64_t no_anomaly_count_;
+  mutable std::atomic<uint64_t> no_anomaly_count_;
 };
 
 }  // namespace ttts
