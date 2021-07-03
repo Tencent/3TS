5c474a1a9a8991dd3156e55d5882ad8519e20341
xenit13 <xenit@foxmail.com>
2021-07-03 18:59:44 +0800

    Fix row_lock

--- contrib/deneva/concurrency_control/row_lock.cpp
+++ contrib/deneva/concurrency_control/row_lock.cpp
@@ -244,7 +244,7 @@ RC Row_lock::lock_release(TxnManager * txn, access_t type) {
     if(type == XP){
         if (STATS_ENABLE && simulation->is_warmup_done()) stats._stats[txn->get_thd_id()]->txn_abort_time -= get_sys_clock() - starttime;
     } else{
-        if (STATS_ENABLE && simulation->is_warmup_done()) stats._stats[txn->get_thd_id()]->txn_abort_time -= get_sys_clock() - starttime;
+        if (STATS_ENABLE && simulation->is_warmup_done()) stats._stats[txn->get_thd_id()]->txn_update_manager_time -= get_sys_clock() - starttime;
     }
     
     DEBUG("unlock (%ld,%ld): owners %d, own type %d, key %ld %lx\n", txn->get_txn_id(),
