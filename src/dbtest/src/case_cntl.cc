9a1dcec0ebc4d10e8b29bf8506d792fcb4626f38
zzhuncle <3294175303@qq.com>
2023-09-21 15:01:10 +0800

    add comments and docs

--- src/dbtest/src/case_cntl.cc
+++ src/dbtest/src/case_cntl.cc
@@ -12,6 +12,17 @@
 Outputter outputter;
 ResultHandler result_handler;
 
+/**
+ * Parses a given line to extract the execution order ID, transaction ID, and SQL statement.
+ * 
+ * The function expects the input line to be formatted with dashes ("-") separating the 
+ * execution order ID, the transaction ID, and the SQL statement. If the format is not met, 
+ * an error message is displayed. The function then returns a vector containing the extracted elements.
+ * 
+ * @param line The input string containing the execution order ID, transaction ID, and SQL statement.
+ * @return A vector of strings where the first element is the execution order ID,
+ *         the second is the transaction ID, and the third is the SQL statement.
+ */
 std::vector<std::string> CaseReader::TxnIdAndSql(const std::string& line) {
     std::vector<std::string> txn_id_and_sql;
     const auto index_first = line.find("-");
@@ -28,6 +39,23 @@ std::vector<std::string> CaseReader::TxnIdAndSql(const std::string& line) {
     return txn_id_and_sql;
 }
 
+/**
+ * Parses a given line to extract the SQL ID and its expected result.
+ * 
+ * The function expects the input line to be formatted with a dash ("-") separating the 
+ * SQL ID from the result. The result can be a space-separated list of values. The function 
+ * will then return a pair where the first element is the SQL ID (as an integer) and the 
+ * second element is the enhanced result string. Each value in the result is enclosed within 
+ * parentheses, except for 'null' values.
+ * 
+ * For example, given a line "9-0,1 1,1", the function will return a pair with 9 as the SQL ID 
+ * and "(0,1) (1,1)" as the result.
+ * 
+ * If the format is not met or there's an issue with the SQL ID, an appropriate message is displayed.
+ * 
+ * @param line The input string containing the SQL ID and its expected result.
+ * @return A pair where the first element is the SQL ID and the second is the enhanced result string.
+ */
 std::pair<int, std::string> CaseReader::SqlIdAndResult(const std::string& line) {
     std::pair<int, std::string> sql_id_and_result;
     const auto index = line.find("-");
@@ -61,11 +89,43 @@ std::pair<int, std::string> CaseReader::SqlIdAndResult(const std::string& line) 
     return sql_id_and_result;
 }
 
+/**
+ * Extracts the isolation level from the given line.
+ * 
+ * The function expects the input line to contain an isolation level followed by 
+ * a curly brace "{" character. It will extract and return everything before the 
+ * curly brace as the isolation level.
+ * 
+ * For example, given a line "serializable{...}", the function will return "serializable".
+ * 
+ * @param line The input string containing the isolation level and subsequent data enclosed within curly braces.
+ * @return The extracted isolation level from the input string.
+ */
 std::string CaseReader::Isolation(const std::string& line) {
     const auto index = line.find("{");
     return line.substr(0, index - 1);
 }
 
+/**
+ * Parses the provided test file to extract both test sequences and their corresponding expected results.
+ * 
+ * The function reads through the test file line by line to identify SQL transactions, their expected results, 
+ * and additional information like the isolation level and parameter numbers. It constructs two main data structures:
+ * a TestSequence which represents the sequence of transactions, and a TestResultSet which represents the set 
+ * of expected results for each transaction in the sequence.
+ * 
+ * @param test_file Path to the test file to be read.
+ * @param db_type Database type identifier.
+ * 
+ * Expected Format:
+ * - The file may contain lines starting with "#" which are treated as comments and ignored.
+ * - A line with "ParamNum" is used to set the number of parameters for the test sequence.
+ * - The isolation level is expected to be present in a line that contains a "{" character.
+ * - Each transaction's SQL and its expected result are parsed from lines without "{" or "}".
+ * - Each result row is defined between lines with "{" and "}".
+ * 
+ * @return A pair containing the extracted TestSequence and TestResultSet from the test file.
+ */
 std::pair<TestSequence, TestResultSet> CaseReader::TestSequenceAndTestResultSetFromFile(const std::string& test_file, const std::string& db_type) {
     std::pair<TestSequence, TestResultSet> test_sequence_and_result_set;
     std::ifstream test_stream(test_file);
@@ -87,9 +147,11 @@ std::pair<TestSequence, TestResultSet> CaseReader::TestSequenceAndTestResultSetF
                 auto index_left = line.find("{");
                 auto index_right = line.find("}");
                 auto index_anno = line.find("#");
+                // Skip comment lines (those starting with #)
                 if (index_anno != line.npos) {
                     continue;
                 }
+                // Retrieve and set the parameter count (if the line contains "ParamNum")
                 if (index_table_type != line.npos) {
                     auto start = line.find(":");
                     std::string param_num_str = "";
@@ -158,6 +220,20 @@ std::pair<TestSequence, TestResultSet> CaseReader::TestSequenceAndTestResultSetF
     return test_sequence_and_result_set;
 }
 
+/**
+ * Initializes the lists of TestSequence and TestResultSet List based on a provided test path and database type.
+ * 
+ * The function starts by reading the "do_test_list.txt" file. Each line in this file should represent 
+ * a test case. The function will then, for each test case, retrieve the respective test sequence and
+ * test result set by reading the corresponding file named "<test_case>.txt" located in the provided test path.
+ * 
+ * Lines in the "do_test_list.txt" file starting with a '#' are considered comments and are skipped.
+ * If the required test file for a test case is successfully read and parsed, a success message is printed.
+ * 
+ * @param test_path The path to the directory containing the test files.
+ * @param db_type The type of the database being tested (e.g., "MySQL").
+ * @return true if the initialization is successful, false if the "do_test_list.txt" file is not found.
+ */
 bool CaseReader::InitTestSequenceAndTestResultSetList(const std::string& test_path, const std::string& db_type) {
     std::cout << dash + "read test sequence and test result set start" + dash << std::endl;
     std::ifstream do_test_stream("./do_test_list.txt");
@@ -188,6 +264,19 @@ bool CaseReader::InitTestSequenceAndTestResultSetList(const std::string& test_pa
     }
 }
 
+/**
+ * Compares the current SQL result with the expected SQL result.
+ * 
+ * The function checks if the given current result (cur_result) matches the expected result (expected_result) for a specific SQL query.
+ * The comparison is done element-wise for each entry in the result vectors. If any entry is different, or the sizes of the two vectors 
+ * differ, the function returns false, indicating the results are not as expected. Otherwise, it returns true.
+ * 
+ * @param cur_result A vector containing the current SQL query result.
+ * @param expected_result A vector containing the expected SQL query result.
+ * @param sql_id The ID of the SQL statement being compared.
+ * @param sql The actual SQL statement string (currently not used in the function).
+ * @return true if the current result matches the expected result, false otherwise.
+ */
 bool ResultHandler::IsSqlExpectedResult(std::vector<std::string> cur_result, std::vector<std::string> expected_result, const int sql_id, const std::string& sql) {
     if (cur_result.size() != expected_result.size()) {
         //std::cout << "stmt_id:" << sql_id << " The number of data is different, cur_result:" << cur_result.size() << " expected_result: " << expected_result.size() << std::endl;
@@ -204,6 +293,21 @@ bool ResultHandler::IsSqlExpectedResult(std::vector<std::string> cur_result, std
     return true;
 }
 
+/**
+ * Compares the current test result with a set of expected test results.
+ * 
+ * The function checks if the given current test result (cur_result) matches any of the expected results in the provided list of expected result sets.
+ * For each expected result set, it iteratively compares the results of individual SQL queries. If all queries' results in the current result match those 
+ * in one of the expected result sets, the function returns true. If no such matching expected result set is found, it returns false.
+ * 
+ * The function also appends the matching result information to a file specified by test_process_file.
+ * 
+ * @param cur_result An unordered_map containing the current test results, where the key is the SQL ID and the value is the corresponding result vector.
+ * @param expected_result_set_list A vector containing multiple unordered_maps. Each unordered_map represents expected results for various SQL queries.
+ * @param sql_map An unordered_map associating each SQL ID with its actual SQL statement string.
+ * @param test_process_file The name of the file where matching result information will be appended.
+ * @return true if the current test result matches any of the expected result sets, false otherwise.
+ */
 bool ResultHandler::IsTestExpectedResult(std::unordered_map<int, std::vector<std::string>>& cur_result,
                                          std::vector<std::unordered_map<int, std::vector<std::string>>> expected_result_set_list,
                                          std::unordered_map<int, std::string> sql_map, const std::string& test_process_file) {
@@ -233,6 +337,16 @@ bool ResultHandler::IsTestExpectedResult(std::unordered_map<int, std::vector<std
     return false;
 }
 
+/**
+ * Writes the summarized test results to a specified file.
+ * 
+ * This function iterates through each TestResultSet in the given list and extracts the summarized test result for each set. 
+ * It then writes each test case type and its corresponding result to the provided output file. 
+ * Note that the detailed reasons for each result are stripped; only the core result is written.
+ * 
+ * @param test_result_set_list A vector of TestResultSet objects, each representing the results of a test case.
+ * @param ret_file The path and filename where the summarized results should be written.
+ */
 void Outputter::WriteResultTotal(std::vector<TestResultSet> test_result_set_list, const std::string& ret_file) {
     std::ofstream out(ret_file);
     for (auto& test_result_set : test_result_set_list) {
@@ -244,9 +358,26 @@ void Outputter::WriteResultTotal(std::vector<TestResultSet> test_result_set_list
     out.close();
 }
 
+/**
+ * Compares the current SQL result to a set of expected results, and outputs the comparison to both the console and a file.
+ * 
+ * For each transaction or session, the function uses session_id to create a string "blank" which acts as an indentation for the output.
+ * The current SQL result and its corresponding expected results are then printed and written to the specified file, 
+ * with the results that match being indicated with an asterisk (*) prefix.
+ * 
+ * @param cur_result A vector containing the current SQL result.
+ * @param expected_result_set_list A vector of unordered maps, each representing a set of expected results indexed by SQL ID.
+ * @param sql_id The unique identifier for the SQL statement.
+ * @param sql The actual SQL statement string.
+ * @param session_id The identifier for the current session or transaction, used for output formatting.
+ * @param test_process_file The path to the file where results should be written.
+ * 
+ * @return True if the results are successfully written to the file, otherwise false (e.g., if the file is not found).
+ */
 bool Outputter::PrintAndWriteTxnSqlResult(std::vector<std::string> cur_result,
                                           std::vector<std::unordered_map<int, std::vector<std::string>>> expected_result_set_list,
                                           const int sql_id, const std::string& sql, const int session_id, const std::string& test_process_file) {
+    // blank ensures that each transaction or session has its specific indentation.
     std::string blank(blank_base*(session_id - 1), ' ');
     std::ofstream test_process(test_process_file, std::ios::app);
     if (test_process) {
@@ -295,6 +426,14 @@ bool Outputter::PrintAndWriteTxnSqlResult(std::vector<std::string> cur_result,
     }
 }
 
+/**
+ * Appends the given test case type to a specified file. 
+ * The test case type is formatted with dashes before and after it.
+ *
+ * @param test_case_type The type of the test case to be written.
+ * @param test_process_file The file to which the test case type will be appended.
+ * @return Returns true if the write operation is successful; otherwise returns false.
+ */
 bool Outputter::WriteTestCaseTypeToFile(const std::string& test_case_type, const std::string& test_process_file) {
     std::ofstream test_process(test_process_file, std::ios::app);
     if (test_process) {
@@ -306,6 +445,14 @@ bool Outputter::WriteTestCaseTypeToFile(const std::string& test_case_type, const
     }
 }
 
+/**
+ * Appends the given result type to a specified file. 
+ * The result type is prefixed with "Test Result:" for clarity.
+ *
+ * @param result_type The result type to be written.
+ * @param test_process_file The file to which the result type will be appended.
+ * @return Returns true if the write operation is successful; otherwise returns false.
+ */
 bool Outputter::WriteResultType(const std::string& result_type, const std::string& test_process_file) {
     std::ofstream test_process(test_process_file, std::ios::app);
     if (test_process) {
--- src/dbtest/src/common.cc
+++ src/dbtest/src/common.cc
@@ -12,15 +12,43 @@
 std::string dash(10, '-');
 int blank_base = 40;
 
+/**
+ * Removes all trailing whitespace characters from the given string str.
+ * 
+ * This function aims to remove all whitespace characters such as spaces, tabs, enters, etc., from the given string str.
+ * It utilizes the remove_if function, which moves all non-whitespace characters to the front of the string and then returns an iterator pointing to the first removed element.
+ * Subsequently, the erase function is used to delete all characters starting from that iterator to the end of the string.
+ * 
+ * @param str The string from which trailing whitespace characters are to be removed.
+ */
 void TrimSpace(std::string& str) {
     auto itor = remove_if(str.begin(), str.end(), ::isspace);
     str.erase(itor, str.end());
 }
 
+/**
+ * Removes all double quote (") characters from the given string str.
+ * 
+ * This function is designed to remove all occurrences of the double quote character from the specified string str.
+ * 
+ * @param str The string from which the double quote characters are to be removed.
+ */
 void TrimQuo(std::string& str) {
     str.erase(std::remove(str.begin(), str.end(), '\"'), str.end());
 }
 
+/**
+ * Splits the given string str by the delimiter delim and stores the results in the tokens vector.
+ * 
+ * This function aims to split the specified string str based on the delimiter delim. The resulting substrings are then 
+ * stored in the tokens vector. Initially, the tokens vector is cleared to ensure no pre-existing data remains.
+ * To achieve the splitting, the function uses two iterators, start and position, which cycle through the string 
+ * to identify and extract substrings separated by the delimiter. These substrings are then appended to the tokens vector.
+ * 
+ * @param str The string to be split.
+ * @param tokens A vector to hold the split substrings.
+ * @param delim The delimiter used for splitting the string.
+ */
 void split(const std::string& str, std::vector<std::string>& tokens, const std::string delim) {
     tokens.clear();
     auto start = str.find_first_not_of(delim, 0);
