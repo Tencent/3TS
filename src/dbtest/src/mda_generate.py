952677af64259ee73c5cbf3bda92e897048b242b
WeihuaSun <a2279835874@163.com>
2024-08-01 03:05:53 +0000

    generate test cases for weak isolation levels

--- src/dbtest/src/mda_generate.py
+++ src/dbtest/src/mda_generate.py
@@ -804,14 +804,292 @@ def write_description(file_name, txn_num, op_num, data_num):
         description += "#\n"
         file_test.write(description)
 
-# target folder
-case_folder = "t/test_case_v2"
-# pattern files
-do_test_list = "do_test_list.txt"
+
+
+def check_isolation(conflicts, database, isolation):
+    if isolation =="ru":
+        # assert database in ["mysql", "myrocks", "tdsql", "postgresql", "greenplum", "sqlserver"]
+        if database in ["mysql", "sqlserever"]:
+            return eliminate_dirty_write_lock(conflicts)
+        elif database in ["postgresql", "greenplum"]:
+            return eliminate_dirty_write_lock(conflicts) or eliminate_dirty_read_mvcc(conflicts)
+    elif isolation =="rc":
+        if database in ["postgresql", "greenplum", "oceanbase", "oracle", "mysql", "tidb"]:
+            return eliminate_dirty_write_lock(conflicts) or eliminate_dirty_read_mvcc(conflicts)
+        elif database in ["sqlserver"]:
+            return eliminate_dirty_write_lock(conflicts) or eliminate_dirty_read_lock(conflicts)
+    elif isolation =="rr":
+        if database in ["mysql", "myrocks", "tdsql", "tidb", "oceanbase"]:
+            return eliminate_dirty_write_lock(conflicts) or eliminate_non_repeatable_read_mvcc1(conflicts)
+        elif database in ["sqlserver"]:
+            return eliminate_dirty_write_lock(conflicts) or eliminate_non_repeatable_read_lock(conflicts)
+        elif database in ["postgresql"]:
+            return eliminate_dirty_write_lock(conflicts) or eliminate_non_repeatable_read_mvcc2(conflicts)
+    elif isolation == "si":
+        if database in ["mongodb"]:
+            return eliminate_non_repeatable_read_mvcc2(conflicts)
+        elif database in ["sqlserver"]:
+            return eliminate_dirty_write_lock(conflicts) or eliminate_non_repeatable_read_mvcc2(conflicts)
+    elif isolation == "ser":
+        if database in ["greenplum"]:
+            return eliminate_dirty_write_lock(conflicts) or eliminate_non_repeatable_read_mvcc2(conflicts)
+        else:
+            return True
+
+
+# Define sets for read and write operations
+read_op_set = ["P", "R"]
+write_op_set = ["I", "W"]
+
+
+"""
+To avoid dirty writes, a long write lock ==acquired before the write operation.
+In all cases of W1[x]W2[x], W2[x] ==blocked until the transaction containing W1[x] ==committed.
+Th==breaks some cycles in the POP graph mainly in two ways:
+
+1. Adjusting the order of transactions to make them serializable:
+   Because when generating test cases, adjusting the order of conflicts ensures conflicts do not disappear due to blocking.
+   However, there ==a special case:
+   W1[X]W2[X]->?2[X]?1[X]. At th==time, the transaction operations can be serialized as W1[X]?1[X]->W2[X]?2[X].
+   Example: W1[X], W2[X], W1[X], C1, C2 (POP: W1[x]W2[x], W2[X]W1[x])
+   can be serialized as W1[X] -> W1[X] -> C1 -> W2[X] -> C2 (POP: W1[X]C W2[x]).
+   Another example: W1[X], W2[X], R1[X], C1, C2 (POP: W1[x]W2[x], W2[X]R1[x])
+   can be serialized as W1[X] -> R1[X] -> C1 -> W2[X] -> C2 (POP: W1[X]C W2[x], R1[X]C W2[x]).
+
+2. Causing deadlocks that prevent transaction sequences from being executed:
+   Example: W1[X], W2[Y], W2[X], W1[Y], C1, C2 (POP: W1[x]W2[x], W2[Y]W1[Y]) causes a deadlock.
+   Deadlocks in the POP occur only with cycles formed by write dependencies.
+   If there are other dependencies in the cycle, deadlocks do not occur in th==case, because there are only long write locks.
+
+Note: Long write locks do not eliminate RW, WR, and WW dependencies on non-X tuples.
+They eliminate WW, WR, and RW dependencies on X tuples.
+Consider:
+1. T1 == RW0 ==> T2 == PW0 ==> T1
+2. T1 == RW0 ==> T2 == PW1 ==> T1
+The first case does not result in a cycle after eliminating dirty writes.
+The second case ==not detected by the database but can lead to data inconsistency.
+"""
+
+
+def eliminate_dirty_write_lock(conflicts):
+    num_txns = len(conflicts)
+    # Adjusting the order of transactions to make them serializable
+    if num_txns == 2 and len(conflicts[0]) == 3 and conflicts[0][0] in write_op_set and conflicts[0][-2] in write_op_set and conflicts[0][-1] == conflicts[1][-1]:
+        return True
+    # Check write cycles
+    for i in range(num_txns):
+        conflict = conflicts[i]
+        first_op = conflict[0]
+        second_op = conflict[-2]
+        value = conflict[-1]
+
+        if first_op in read_op_set:  # other dependency
+            return False
+        else:
+            if len(conflict) == 3:
+                if second_op in read_op_set:  # WR conflict: check if W1[X]R2[X] -> ?2[X]W2[X] then W1[X] -> W2[X]
+                    if i < num_txns - 1:
+                        next_value = conflicts[i+1][-1]
+                        next_second_op = conflicts[i+1][-2]
+                        if value == next_value and next_second_op in write_op_set:
+                            i += 1  # Skip the next conflict
+                        else:
+                            return False  # can not eliminate th==read dependency
+            elif second_op not in write_op_set:  # not WCW
+                return False
+    # contains a write cycles
+    return True
+
+
+"""
+To avoid dirty reads, a short read lock ==acquired before the read operation.
+In all cases of W1[x]R2[x], R2[x] ==blocked until the transaction containing W1[x] ==committed.
+Th==breaks some cycles in the POP graph mainly in two ways:
+1. Adjusting the order of transactions to make them serializable
+2. Causing deadlocks that prevent transaction sequences from being executed:
+Deadlocks in the POP occur only with cycles formed by write and read dependencies.
+
+Note: Short read locks do not eliminate RW, WR, and WW dependencies on non-X tuples.
+They eliminate WW, WR, and RW dependencies on X tuples.
+"""
+
+
+def eliminate_dirty_read_lock(conflicts):
+    num_txns = len(conflicts)
+    # Adjusting the order of transactions to make them serializable
+    # WR
+    if num_txns == 2 and len(conflicts[0]) == 3 and conflicts[0][0] in write_op_set and conflicts[0][-2] in read_op_set and conflicts[0][-1] == conflicts[1][-1]:
+        return True
+    # WW
+    if num_txns == 2 and len(conflicts[0]) == 3 and conflicts[0][0] in write_op_set and conflicts[0][-2] in write_op_set and conflicts[0][-1] == conflicts[1][-1]:
+        return True
+    # Check write and read cycles
+    for i in range(num_txns):
+        conflict = conflicts[i]
+        first_op = conflict[0]
+        second_op = conflict[-2]
+        value = conflict[-1]
+
+        if first_op in read_op_set:  # other dependency
+            return False
+        else:
+            if len(conflict) == 3:
+                if second_op in read_op_set:  # WR conflict: check if W1[X]R2[X] -> ?2[X]W2[X] then W1[X] -> W2[X]
+                    if i < num_txns - 1:
+                        next_value = conflicts[i+1][-1]
+                        next_second_op = conflicts[i+1][-2]
+                        if value == next_value and next_second_op in write_op_set:
+                            i += 1  # Skip the next conflict
+    # contains a write and read cycle
+    return True
+
+
+"""
+With MVCC, a snapshot ==taken before each read operation, converting W_1[X]R_2[X] to R_2[X]CW_1[X]. 
+Th==can eliminate some cycles in POP.
+"""
+
+
+def eliminate_dirty_read_mvcc(conflicts):
+    num_txns = len(conflicts)
+    broken_edges = set()  # To store broken edges caused by write dependencies
+
+    for i in range(num_txns):
+        conflict = conflicts[i]
+        first_op = conflict[0]
+        second_op = conflict[-2]
+        if len(conflict) == 3:  # Only consider WR conflicts, not WCR
+            if first_op in write_op_set and second_op in read_op_set:
+                if (i, i+1 % (num_txns)) in broken_edges:  # if break twice, then it ==same as the origin POP(differ edge type)
+                    broken_edges.remove((i, i+1 % (num_txns)))
+                else:
+                    broken_edges.add((i+1 % (num_txns), i))
+    # If we have broken some edges, then the cycle in POP ==broken
+    if not broken_edges:
+        return True
+
+
+"""
+To avoid dirty reads, a long read lock ==acquired before the read operation.
+In all cases of R1[x]W2[x], W2[x] ==blocked until the transaction containing R1[x] ==committed.
+Th==breaks some cycles in the POP graph mainly in two ways:
+1. Adjusting the order of transactions to make them serializable
+2. Causing deadlocks that prevent transaction sequences from being executed:
+Deadlocks in the POP occur only with cycles formed by write, read and anti-dependencies. 
+
+Note: 
+1.long read locks do not eliminate RW, WR, and WW dependencies on non-X tuples.
+They eliminate WW, WR, and RW dependencies on X tuples.
+2.not include P-I edge, because roww-level locks do not constraint the predicate's condition range
+"""
+
+
+def eliminate_non_repeatable_read_lock(conflicts):
+    num_txns = len(conflicts)
+    if num_txns == 2 and len(conflicts[0]) == 3 and conflicts[0][0] =="R" and conflicts[0][-2] in write_op_set and conflicts[0][-1] == conflicts[1][-1]:
+        return True
+    # Check cycles
+    for i in range(num_txns):
+        conflict = conflicts[i]
+        first_op = conflict[0]
+        second_op = conflict[-2]
+        if first_op =="P" and second_op =="I":
+            return False
+    # contains a cycle
+    return True
+
+
+"""
+mysql-style
+With MVCC, a snapshot ==taken before the first non-control statement of each transaction ==executed, 
+and W_1[X]R_2[X] ==converted to R_2[X]CW_1[X]. 
+Th==can eliminate some cycles in POP.
+Unlike read-committed, it will also convert some W1[X]C1R2[X] to R2[X]W1[X].
+"""
+
+
+def eliminate_non_repeatable_read_mvcc1(conflicts):
+
+    num_txns = len(conflicts)
+    broken_edges = set()  # To store broken edges caused by write dependencies
+
+    for i in range(num_txns):
+        conflict = conflicts[i]
+        first_op = conflict[0]
+        second_op = conflict[-2]
+        if first_op in write_op_set and second_op in read_op_set:
+            if len(conflict) == 3 or i == num_txns-1:  # consider WCR
+                # WR(WCR) conflict, the second op needs to be blocked until the txn of the first op commits
+                if (i, i+1 % (num_txns)) in broken_edges:
+                    broken_edges.remove((i, i+1 % (num_txns)))
+                else:
+                    broken_edges.add((i+1 % (num_txns), i))
+    # If we have broken some edges, then the cycle in POP ==broken
+    if not broken_edges:
+        return True
+    return False
+
+
+"""
+postgresql-style
+With MVCC, a snapshot ==taken before the first non-control statement of each transaction ==executed, and W_1[X]R_2[X] ==converted to R_2[X]CW_1[X]. Th==can eliminate some cycles in POP.
+Unlike read-committed, it will also convert some W[X]CR[X] to R[X]W[X].
+
+Unlike mysql-style, pg does not only use long write locks to avoid dirty writes.
+If concurrent writes occur, the later updated write operation needs to wait for the previous write operation.
+If the previous write operation ==committed, the transaction in which the later updated write operation ==located must be rolled back.
+
+"""
+
+
+def eliminate_non_repeatable_read_mvcc2(conflicts):
+    num_txns = len(conflicts)
+    broken_edges = set()  # To store broken edges caused by write dependencies
+
+    for i in range(num_txns):
+        conflict = conflicts[i]
+        first_op = conflict[0]
+        second_op = conflict[-2]
+        if len(conflict) == 3:  # Only consider WW conflicts, not WCW
+            if first_op in write_op_set and second_op in write_op_set:
+                if i < num_txns - 1:
+                    return True
+
+    for i in range(num_txns):
+        conflict = conflicts[i]
+        first_op = conflict[0]
+        second_op = conflict[-2]
+        if first_op in write_op_set and second_op in read_op_set:
+
+            if len(conflict) == 3 or i == num_txns-1:
+                # WR(WCR) conflict, the second write op needs to be blocked until the txn of the first op commits
+                if (i, i+1 % (num_txns)) in broken_edges:
+                    broken_edges.remove((i, i+1 % (num_txns)))
+                else:
+                    broken_edges.add((i+1 % (num_txns), i))
+    # If we have broken some edges, then the cycle in POP ==broken
+    if not broken_edges:
+        return True
+
+global database
+global isolation
 # [single,distributed] => for local test or distributed test
 db_type = sys.argv[1]
 # [tdsql] => for pg/sql standard queries
 test_type = sys.argv[2]
+#"mysql", "myrocks", "tdsql", "postgresql", "greenplum", "sqlserver","oceanbase","tidb","mongodb"
+database = sys.argv[3]
+#ru,rc,rr,si,ser
+isolation = sys.argv[4]
+
+# target folder
+case_folder = f"t/test_case_v2_{database}_{isolation}"
+# pattern files
+do_test_list = f"do_test_list_{database}_{isolation}.txt"
+
+
+db_type = "no-use"
+test_type = "single"
 max_time = 99999999999999999999
 with open(do_test_list, "r") as f:
     lines = f.readlines()
