954d877d00a409d69a86a99cc03206382ebdda72
Yuxing Chen <axinggu@qq.com>
2025-07-08 10:08:12 +0800

    Update License
    required by Tencent

--- contrib/deneva/benchmarks/ycsb_query.cpp
+++ contrib/deneva/benchmarks/ycsb_query.cpp
@@ -1,9 +1,7 @@
 /*
      Tencent is pleased to support the open source community by making 3TS available.
 
-     Copyright (C) 2020 THL A29 Limited, a Tencent company.  All rights reserved. The below software
-     in this distribution may have been modified by THL A29 Limited ("Tencent Modifications"). All
-     Tencent Modifications are Copyright (C) THL A29 Limited.
+     Copyright (C) 2020 Tencent.  All rights reserved.
 
      Author: hongyaozhao@ruc.edu.cn
 
